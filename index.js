be9f8d241c5307e7c5fd95956568457040ba892d
Spikyz <35696012+Spikyz@users.noreply.github.com>
2018-01-22 16:40:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -91,3 +91,116 @@ function story_random(min, max) {
         max = Math.floor(storynumber);
         randnum = Math.floor(Math.random() * (max - min +1) + min);
 }
+
+//Captcha
+
+
+const client = new Discord.Client();
+
+
+client.on("ready", () => {
+
+var TailleMembres = client.users.size;
+var TailleServeurs = client.guilds.size;
+
+client.user.setGame("CAPTCHA BOT EN LIGNE");
+
+console.log("--------------------------------------");
+
+console.log(`Je suis sur ${TailleServeurs} serveur(s) avec ${TailleMembres} utilisateurs`);
+
+});
+
+
+var Attente = [];
+
+
+client.on('message', (msg) => {
+
+  if(msg.channel.name == "certification"){
+
+  if(msg.author !== client.user) {
+
+    msg.delete();
+
+  }else{
+
+    msg.channel.send();
+
+  }
+}
+
+});
+
+
+client.on('message', (msg) => {
+
+if(msg.author.id != client.id){
+
+if(msg.channel.name == "certification" && msg.content.startsWith(prefix)){
+
+        var say = msg.content.substr(1);
+
+        for(i=0;i<Attente.length;i++){
+
+            var code = Attente[i].indexOf("x");
+
+        }
+
+        code++;
+
+        for(i=0;i<Attente.length;i++){
+
+var recode = Attente[i].substr(code);
+
+}
+		if(say == recode){
+
+            Attente.pop();     
+
+let NouveauMembre = msg.guild.roles.find("name", "Nouveau 🎓");
+
+if(!msg.guild.roles.exists("name", "Nouveau 🎓")) {
+
+        return  msg.channel.send("**:x: Le role `Nouveau 🎓` n'existe pas, veuillez le créer pour faire fonctionner le captcha!**")
+     
+      } 
+msg.member.addRole(NouveauMembre).catch(err => console.log(err));   
+
+        }else{
+
+            msg.author.send("**:x: Vous avez pas réussi à passer le captcha...**");
+
+            msg.delete();
+
+            if(!msg.guild.member(client.user).hasPermission("KICK_MEMBERS")) {
+
+  return;
+}
+
+msg.guild.member(msg.author).kick();
+
+		}
+  }
+}
+
+});
+
+client.on('guildMemberAdd', member => {  
+
+const salon = member.guild.channels.find('name', 'passage');
+
+    if(!salon) return;
+
+var captcha = String(Math.random()).charAt(4) + String(Math.random()).charAt(4) + String(Math.random()).charAt(4) + String(Math.random()).charAt(4) + String(Math.random()).charAt(4);
+           
+            member.send("**Bienvenue @"+ member.user.username + "** copie/colle le code ci-join dans **#certification** pour passer le captcha du serveur\n```." + captcha + "```");
+        
+            member.user.id;
+         
+            Attente.push(member.user.id + "x" + captcha);
+
+    salon.send("**Bienvenue @"+ member.user.username + "**"); 
+
+});
+
