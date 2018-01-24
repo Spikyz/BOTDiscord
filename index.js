f54264661faf7b6e5c27bdf637b39ce676c521cc
Spikyz <35696012+Spikyz@users.noreply.github.com>
2018-01-24 23:44:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -83,7 +83,23 @@ bot.on('message', message => {
         console.log("Commande //test demandée !");
 
     }
-
+    
+    if (message.content === prefix + "promote"){
+        var promote_embed = new Discord.RichEmbed()
+            .setColor('#84815B')
+            .setTitle("Promote !")
+            .addField("args.join(" ") est désormais promu !")
+        message.channel.sendEmbed(promote_embed);
+        console.log("Commande //promote demandée !");
+    }
+    
+    if (message.content === prefix + "demote"){
+        var demote_embed = new Discord.RichEmbed()
+            .setColor('#f1c40f')
+            .addField("Bot Ready !", "Le bot est Opérationnel")
+        message.channel.sendEmbed(test_embed);
+        console.log("Commande //demote demandée !");
+    }
     if (message.content === prefix + "inviteme"){
         message.channel.send("Voici notre Discord ! \n - **https://discord.me/henosia**");
         console.log("Commande //inviteme demandée !");
