d569ca99ad6d947aca702f94a912c17d7e1b9fe0
Spikyz <35696012+Spikyz@users.noreply.github.com>
2018-01-29 20:12:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,14 +5,8 @@ const FileSync = require('lowdb/adapters/FileSync')
 const adapter = new FileSync('database.json');
 const db = low(adapter);
 
-db.defaults({ histoires: []})
-    .write()
-
 var bot = new Discord.Client();
 var prefix = ("//");
-var randnum = 0;
-
-var storynumber = db.get('histoires').map('story_value').value();
 
 bot.on('ready', () => {
     bot.user.setPresence({ game: { name: '[//help] HenosiaBOT', type: 0} });
@@ -25,47 +19,6 @@ bot.on('message', message => {
 
     var msgauthor = message.author.id;
 
-    if (message.author.bot)return;
-
-    if (message.content === "ping"){
-        message.reply("pong");
-        console.log("ping pong !");
-    }
-
-    if (message.content === "Bonjour"){
-        message.reply("Bonjour");
-    }
-
-    if (!message.content.startsWith(prefix)) return;
-    var args = message.content.substring(prefix.length).split(" ");
-
-    switch (args[0].toLowerCase()){
-
-        case "newstory":
-        var value = message.content.substring(10);
-        var author = message.author.id;
-        console.log(value);
-        message.reply("Ajout de l'histoire a la base de données")
-
-        db.get('histoires')
-            .push({ story_value: value, story_author: author})
-            .write();
-        break;
-
-        case "tellstory":
-
-        story_random();
-        console.log(randnum);
-
-        var story = db.get('histoires[${randnum}].story_value').toString().value();
-        var author_story = db.get('histoires[${randnum}].author').toString().value();
-        console.log(story);
-
-        message.channel.send('Histoire : ${story} Histoire de ${author_story})') 
-
-        break;
-    }
-
     if (message.content === prefix + "help"){
         var help_embed = new Discord.RichEmbed()
             .setColor('#f1c40f')
@@ -76,7 +29,7 @@ bot.on('message', message => {
      if (message.content === prefix + "info"){
         var info_embed = new Discord.RichEmbed()
             .setColor('#f1c40f')
-            .addField("***Information:***", "**Nom** \n \n HenosiaBOT \n **Prefix de base** \n // \n **Auteur** \n @Spikyz™#4563 \n **Statue** \n En développement ( 5% ) \n **Library** \n Discord.js \n **Version** \n 0.0.1")
+            .addField("***Information:***", "**Nom** \n HenosiaBOT \n \n **Prefix de base** \n // \n \n **Auteur** \n @Spikyz™#4563 \n \n **Statue** \n En développement ( 5% ) \n \n **Library** \n Discord.js \n \n **Version** \n 0.0.1")
         message.channel.sendEmbed(info_embed);
         console.log("Commande //info demandée !");
    }
@@ -118,9 +71,3 @@ bot.on('message', message => {
       console.log("Commande //espace demandée !");
     }
     });
-    
-function story_random(min, max) {
-        min = Math.ceil(1);
-        max = Math.floor(storynumber);
-        randnum = Math.floor(Math.random() * (max - min +1) + min);
-}
