4309735f69d030f1f09bc7e957b9486e1f2c357d
Spikyz <35696012+Spikyz@users.noreply.github.com>
2018-03-01 12:49:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const tokenfile = require("./token.json");
 
 bot.on('ready', function () {
   console.log('Je suis connecté !')
+  bot.user.setActivity("Théo - theobot.cf", {type: "WATCHING"});
 })
 
 bot.login(tokenfile.token);
@@ -21,12 +22,62 @@ bot.on('message', message => {
   if(cmd === `<@418338601382969345>`){
     message.reply("Mon préfix est ``th!``. **th!aide** pour voir mes commandes")
   }
+
+  if(cmd === `${prefix}ban`){
+
+    let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if(!bUser) return message.channel.send("Je ne trouve pas l'utilisateur.");
+    let bReason = args.join(" ").slice(22);
+    if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.channel.send("Tu n'as pas la permissions (< MANAGE_MESSAGE )");
+    if(bUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Tu ne peux pas kick cette utilisateur !");
+
+    let banEmbed = new Discord.RichEmbed()
+    .setDescription("~Exclusion~")
+    .setColor("#bc0000")
+    .addField("Joueur exclu", `${bUser} avec l'ID ${bUser.id}`)
+    .addField("Exclu par", `<@${message.author.id}> avec l'ID ${message.author.id}`)
+    .addField("Salon", message.channel)
+    .addField("Date", message.createdAt)
+    .addField("Raison", bReason);
+
+    let BanChannel = message.guild.channels.find(`name`, "mod-logs");
+    if(!BanChannel) return message.channel.send("Je ne trouve pas le salon **#mod-logs**.");
+
+    message.guild.member(bUser).ban(bReason);
+    BanChannel.send(banEmbed);
+
+  }
+
+  if(cmd === `${prefix}kick`){
   
+    let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if(!kUser) return message.channel.send("Je ne trouve pas l'utilisateur.");
+    let kReason = args.join(" ").slice(22);
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Tu n'as pas la permissions (< MANAGE_MESSAGE )");
+    if(kUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Tu ne peux pas kick cette utilisateur !");
+
+    let kickEmbed = new Discord.RichEmbed()
+    .setDescription("~Expulsion~")
+    .setColor("#e56b00")
+    .addField("Joueur expulsé", `${kUser} avec l'ID: ${kUser.id}`)
+    .addField("Expulsé par", `<@${message.author.id}> avec l'ID: ${message.author.id}`)
+    .addField("Salon", message.channel)
+    .addField("Date", message.createdAt)
+    .addField("Raison", kReason);
+    
+    let kickChannel = message.guild.channels.find(`name`, "mod-logs");
+    if(!kickChannel) return message.channel.send("Je ne trouve pas le salon **#mod-logs**.");
+
+    message.guild.member(kUser).kick(kReason);
+    kickChannel.send(kickEmbed);
+  }
+
+
   if(cmd === `${prefix}report`){
 
     let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     if(!rUser) return message.channel.send("Je ne trouve pas l'utilisateur.");
-    let reason = args.join(" ").slice(22);
+    let Rreason = args.join(" ").slice(22);
 
     let reportEmbed = new Discord.RichEmbed()
     .setDescription("_Signalement_")
@@ -35,10 +86,10 @@ bot.on('message', message => {
     .addField("Signalé par", `${message.author} avec l'ID: ${message.author.id}`)
     .addField("Salon", message.channel)
     .addField("Date", message.createdAt)
-    .addField("Raison", reason);
+    .addField("Raison", Rreason);
 
     let reportschannel = message.guild.channels.find(`name`, "reports-logs");
-    if(!reportschannel) retur, message.channel.send("Je ne trouve pas le canal **#reports-logs**.")
+    if(!reportschannel) retur, message.channel.send("Je ne trouve pas le salon **#reports-logs**.")
 
     message.delete().catch(O_o=>{});
     reportschannel.send(reportEmbed);
